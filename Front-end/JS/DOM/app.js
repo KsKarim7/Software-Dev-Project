ccf701256c42cd599ef4d8215f8d0ed27cc2d6cc
Khaled Karim <kskarim177@gmail.com>
2024-08-06 23:58:27 +0600

    DOM

--- Front-end/JS/DOM/app.js
+++ Front-end/JS/DOM/app.js
@@ -1,14 +1,36 @@
 // alert("Hilu")
-console.log("Hilu");
+// console.log("Hilu");
 var head = document.getElementsByTagName("h1");
-console.log(head);
+// console.log(head);
 // console.log(document.getElementsByTagName("h1"));
 
 var title = document.getElementById('title').style.backgroundColor = 'grey';
 
 document.getElementById('title').style.removeProperty('backgroundColor');
 
-console.log(title);
+// console.log(title);
 
 var child = document.getElementsByClassName('child');
-console.log(child[0]);
+// console.log(child[0]);
+
+
+var testDiv = document.getElementsByClassName("test");
+
+// console.log(testDiv[0].childNodes[1].parentNode.parentNode.parentNode);
+
+var newDiv = document.getElementById("newDiv");
+function createEl() {
+    var p = document.createElement("p");
+    p.innerText = "New BD";
+    newDiv.appendChild(p);
+}
+
+
+createEl();
+
+document.getElementById("submit-btn").addEventListener("click", function (e) {
+    // console.log("funk your browser");
+    // createEl();
+    var inputValue = document.getElementById("input").value;
+    console.log(inputValue);
+})
