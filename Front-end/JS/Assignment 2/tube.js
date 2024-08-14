4b9186dff1aa100eff4dd79df2d6a942d463a8bd
Khaled Karim <kskarim177@gmail.com>
2024-08-15 01:46:01 +0600

    items-container fetched and added from the API

--- Front-end/JS/Assignment 2/tube.js
+++ Front-end/JS/Assignment 2/tube.js
@@ -1,13 +1,13 @@
-const loadData = () => {
+const loadNavData = () => {
     fetch("https://openapi.programming-hero.com/api/videos/categories")
         .then(res => res.json())
-        .then(data => displayData(data.data));
+        .then(data => displayNav(data.data));
 }
 
-loadData();
+loadNavData();
 
 
-const displayData = (data) => {
+const displayNav = (data) => {
     // console.log(data);
 
     const dynamicNav = document.getElementById("dynamic-nav");
@@ -20,3 +20,47 @@ const displayData = (data) => {
         dynamicNav.appendChild(btn);
     });
 };
+
+
+// Load and display items
+
+const loadItemsData = () => {
+    fetch("https://openapi.programming-hero.com/api/videos/category/1000")
+        .then((res) => res.json())
+        .then((data) => displayItemsData(data.data));
+    // .then((data) => console.log(data.data));
+}
+
+loadItemsData();
+
+
+const displayItemsData = (data) => {
+    const displayData = document.getElementById("items-container");
+    data.forEach((item) => {
+        const card = document.createElement("div");
+        card.classList.add("grid-item");
+        card.innerHTML = `
+        <img src="${item.thumbnail}" style="width: 100%; height:60%; border-radius: 5%;" alt="">
+        <div class="d-flex py-3">
+            <img class="" style="width: 8%; height:15%;   border-radius: 50%;" src="${item.authors[0].profile_picture}" alt="">
+            <div class="ms-2 lh-1" style="color: rgb(147, 146, 146);">
+                <p class="fw-bold text-dark">${item.title}</p>
+                <p>${item.authors[0].profile_name} <span>${ifVerified(item.authors[0].verified)}</span></p>
+                <p>${item.others.views}</p>
+            </div>
+        </div>
+        `;
+        displayData.appendChild(card);
+    })
+}
+
+function ifVerified(verified) {
+    if (verified) {
+        return '<i class="fa fa-check-circle ms-2" style="color: #3471da;"></i>';
+    }
+    else {
+        return '';
+    }
+}
+
+
