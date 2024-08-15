87e3adb2a985ad85f836137bcc6fad64451a018f
Khaled Karim <82402301+KsKarim7@users.noreply.github.com>
2024-08-16 02:30:50 +0600

    Delete Back-end/Django codes/room_1/project_1 directory

--- Back-end/Django codes/room_1/project_1/project_1/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("This is the home page");
-
-def contact(request):
-    return HttpResponse("This is a contact request")
