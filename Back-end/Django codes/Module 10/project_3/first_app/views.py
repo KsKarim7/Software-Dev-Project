fd42e9d0f9b216e1ae763e109f832c21b6087fde
Khaled Karim <kskarim177@gmail.com>
2024-08-18 01:37:15 +0600

    django template language and module 10

--- Back-end/Django codes/Module 10/project_3/first_app/views.py
+++ Back-end/Django codes/Module 10/project_3/first_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def home(req):
-    dict = {'author':'Karim','age':23}
+    dict = {'author':'Karim','age':23, 'lst':['python','is','the','best'],'birthday':datetime.datetime.now(),'courses' :[{
+        'id' :1,
+        'name': 'Django',
+        'fee':5000
+    },
+    {
+        'id' :2,
+        'name': 'Nodejs',
+        'fee' : 6000
+    },
+    {
+        'id' :3,
+        'name': 'DSA',
+        'fee' : 3000
+    }
+    ]}
     # return render(req,'home.html',{'author':'Karim','age':23});
     return render(req,'home.html',dict);
