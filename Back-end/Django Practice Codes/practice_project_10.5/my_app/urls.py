11f3dfa59c2c63ca53717eb8352db64a738ce081
Khaled Karim <kskarim177@gmail.com>
2024-08-18 19:22:54 +0600

    Page navigation practice with bootstrap

--- Back-end/Django Practice Codes/practice_project_10.5/my_app/urls.py
+++ Back-end/Django Practice Codes/practice_project_10.5/my_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home),
+    path('kids/',views.kids),
+    path('mens/',views.mens),
+    path('womens/',views.womens),
 ]
--- Back-end/Django Practice Codes/practice_project_10.5/my_app/views.py
+++ Back-end/Django Practice Codes/practice_project_10.5/my_app/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(req):
-    return render(req,'home.html')
+def kids(req):
+    return render(req,'navigation/kids.html')
+
+def mens(req):
+    return render(req,'navigation/mens.html')
+    
+def womens(req):
+    return render(req,'navigation/womens.html')
