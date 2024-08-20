6109e915b843d85f97f1bce3566ed892adab2f47
Khaled Karim <kskarim177@gmail.com>
2024-08-20 16:29:34 +0600

    Here we goo

---
+++ Back-end/Django Practice Codes/Assignment 3/bon_appétit/bon_appétit/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bon_appétit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bon_appétit.settings')
+
+application = get_wsgi_application()
