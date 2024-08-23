2a377a4c55bcac96b743d04b60a74d8396b6fdb4
Khaled Karim <kskarim177@gmail.com>
2024-08-23 22:55:35 +0600

    Deletion of model data from frontend

--- Back-end/Django codes/Module 14/main/first_app/views.py
+++ Back-end/Django codes/Module 14/main/first_app/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . import models
 
 # Create your views here.
 def home(req):
-    return render(req,"home.html")
+    student = models.Student.objects.all()
+    return render(req,"home.html",{'data':student})
+
+def delete_student(req,roll):
+    std = models.Student.objects.get(pk = roll).delete()
+    return redirect("homepage")
