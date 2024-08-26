3092375c1902dfe330b4c519c3803af22778f22b
Khaled Karim <kskarim177@gmail.com>
2024-08-27 04:05:10 +0600

    Stuck and goodnight

--- Back-end/Django Practice Codes/practice_project_15.5/music_mix/music_mix/views.py
+++ Back-end/Django Practice Codes/practice_project_15.5/music_mix/music_mix/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from album.models import Album
+from musician.models import Musician
 
 # Create your views here.
 def home(req):
-    return render(req,'home.html')
+    album = Album.objects.all()
+    # album = Album.objects.filter(musician=req.)
+    musician = Musician.objects.filter(album= album[4])
+    print(musician)
+    # print(album,musician)
+    # return render(req,'home.html',{'album':album},{'musician':musician})
+    return render(req,'home.html',{'data':musician})
