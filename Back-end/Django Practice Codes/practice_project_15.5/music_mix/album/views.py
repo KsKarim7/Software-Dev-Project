46331b1f1e0a900078ad11ad5630de8da3cdc803
Khaled Karim <kskarim177@gmail.com>
2024-08-27 02:36:54 +0600

    Musician and Album form connections implemented with the backend

--- Back-end/Django Practice Codes/practice_project_15.5/music_mix/album/views.py
+++ Back-end/Django Practice Codes/practice_project_15.5/music_mix/album/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from . import forms
+
 
 # Create your views here.
 
 def add_album(req):
-    return render(req,'add_album.html')
+    if(req.method == 'POST'):
+        album_form = forms.AlbumForm(req.POST)
+        if(album_form.is_valid()):
+            album_form.save()
+            return redirect('home')
+    
+    else:
+        album_form = forms.AlbumForm()
+
+    return render(req, 'add_album.html',{'form':album_form})
+ 
--- Back-end/Django Practice Codes/practice_project_15.5/music_mix/musician/views.py
+++ Back-end/Django Practice Codes/practice_project_15.5/music_mix/musician/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import forms
 
 # Create your views here.
 def add_musician(req):
-    musician_form = forms.MusicianForm()
+    if(req.method == 'POST'):
+        musician_form = forms.MusicianForm(req.POST)
+        if(musician_form.is_valid()):
+            musician_form.save()
+            return redirect('add_musician')
+    
+    else:
+        musician_form = forms.MusicianForm()
+
     return render(req, 'add_musician.html',{'form':musician_form})
