0e780e53c123aafae23c07b14d9ed09bb7e360a2
Khaled Karim <kskarim177@gmail.com>
2024-08-27 20:14:58 +0600

    Models relation created

--- Back-end/Django Practice Codes/Assignment 4/task_MS/category/models.py
+++ Back-end/Django Practice Codes/Assignment 4/task_MS/category/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from task.models import Task
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    task = models.ManyToManyField(Task)
+    # task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
--- Back-end/Django Practice Codes/Assignment 4/task_MS/task/models.py
+++ Back-end/Django Practice Codes/Assignment 4/task_MS/task/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    task_description = models.TextField(max_length=500)
+    is_completed = models.BooleanField(default=False)
+    due_date = models.DateField()
+
+
+    def __str__(self):
+        return f"{'✅' if self.is_completed else ''}{self.title}!"
+    
+# def delete_task(req,id):
+
+
