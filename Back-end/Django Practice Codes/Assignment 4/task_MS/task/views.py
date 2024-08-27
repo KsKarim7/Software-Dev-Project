be758a8bc01c399cf60ff841286d3cf1eae3a8d9
Khaled Karim <kskarim177@gmail.com>
2024-08-28 02:35:11 +0600

    Edit & Delete btn implemented

--- Back-end/Django Practice Codes/Assignment 4/task_MS/task/views.py
+++ Back-end/Django Practice Codes/Assignment 4/task_MS/task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from . import forms
+from . import models
 # Create your views here.
 
 def add_task(req):
@@ -12,3 +13,19 @@ def add_task(req):
         task_form = forms.TaskForm()
 
     return render(req,'add_task.html',{'form':task_form})
+
+def edit_task(req,id):
+    task = models.Task.objects.get(id=id)
+    task_form = forms.TaskForm(instance=task)
+    if(req.method == 'POST'):
+        task_form = forms.TaskForm(req.POST,instance = task)
+        if(task_form.is_valid):
+            task_form.save()
+            return redirect('home')
+
+    return render(req,'add_task.html',{'form':task_form})
+
+def delete_task(req,id):
+    task = models.Task.objects.get(pk=id)
+    task.delete()
+    return redirect('home')
