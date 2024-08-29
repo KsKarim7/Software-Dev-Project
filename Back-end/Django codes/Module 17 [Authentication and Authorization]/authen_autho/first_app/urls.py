62bd50422e1c8b50be5ace364c0ae3d11ef6f65f
Khaled Karim <kskarim177@gmail.com>
2024-08-29 20:34:07 +0600

    Change User Data using Builtin Django Forms

--- Back-end/Django codes/Module 17 [Authentication and Authorization]/authen_autho/first_app/urls.py
+++ Back-end/Django codes/Module 17 [Authentication and Authorization]/authen_autho/first_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('signup/',views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
     path('profile/',views.profile, name='profile'),
+    path('pass_change/',views.pass_change, name='passchange'),
+    path('pass_change2/', views.pass_change2, name='passchange2'),
     path('logout/', views.user_logout, name='logout'),
 ]
--- Back-end/Django codes/Module 17 [Authentication and Authorization]/authen_autho/first_app/views.py
+++ Back-end/Django codes/Module 17 [Authentication and Authorization]/authen_autho/first_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .forms import RegisterForm,ChangeUserData
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm, SetPasswordForm
+from django.contrib.auth import authenticate,login,logout,update_session_auth_hash
 
 
 # Create your views here.
@@ -29,7 +29,7 @@ def user_login(req):
     if(not req.user.is_authenticated):
 
         if(req.method == 'POST'):
-            form = AuthenticationForm(request=req, data=req.POST)
+            form = AuthenticationForm(req=req, data=req.POST)
             if form.is_valid():
                 name = form.cleaned_data['username']
                 userpass = form.cleaned_data['password']
@@ -46,11 +46,63 @@ def user_login(req):
     else:
         return redirect('profile')
 
+
 def profile(req):
-    if(req.user.is_authenticated):
-        return render(req,'profile.html',{'user':req.user})
+    if req.user.is_authenticated:
+        if req.method == 'POST':
+            form = ChangeUserData(req.POST, instance=req.user)
+            if form.is_valid():
+                messages.success(req, 'Account updated successfully')
+                form.save()
+        else:
+            form = ChangeUserData(instance=req.user)
+        return render(req, './profile.html', {'form': form})
     else:
-        return redirect('login')
+        return redirect('signup')
 def user_logout(req):
     logout(req)
     return redirect('login')
+
+def pass_change(req):
+    if req.user.is_authenticated:
+        if req.method == 'POST':
+            form = PasswordChangeForm(user=req.user, data=req.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(req, form.user)
+                return redirect('profile')
+        else:
+            form = PasswordChangeForm(user=req.user)
+        return render(req, 'passchange.html', {'form': form})
+    else:
+        return redirect('login')
+    
+
+def pass_change2(req):
+    if req.user.is_authenticated:
+        if req.method == 'POST':
+            form = SetPasswordForm(user=req.user, data=req.POST)
+            if form.is_valid():
+                form.save()
+                update_session_auth_hash(req, form.user)
+                return redirect('profile')
+        else:
+            form = SetPasswordForm(user=req.user)
+        return render(req, './passchange.html', {'form': form})
+    else:
+        return redirect('login')
+
+
+def change_user_data(req):
+    if req.user.is_authenticated:
+        if req.method == 'POST':
+            form = ChangeUserData(req.POST, instance=req.user)
+            if form.is_valid():
+                messages.success(req, 'Account updated successfully')
+                form.save()
+                print(form.cleaned_data)
+        else:
+            form = ChangeUserData()
+        return render(req, './profile.html', {'form': form})
+    else:
+        return redirect('signup')
