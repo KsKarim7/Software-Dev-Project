5b0c7d9e19ca7d87a337e46717ad8f9d53d6db02
Khaled Karim <kskarim177@gmail.com>
2024-08-30 15:34:46 +0600

    some modifications

--- Back-end/Django codes/Module 15 (Blog Project Part 2)/blog_project/blog_project/urls.py
+++ Back-end/Django codes/Module 15 (Blog Project Part 2)/blog_project/blog_project/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('author/',include('author.urls')),
     path('post/',include('posts.urls')),
     path('category/',include('categories.urls')),
+    path('category/<slug:category_slug>/', views.home, name='category_wise_post'),
 ]
