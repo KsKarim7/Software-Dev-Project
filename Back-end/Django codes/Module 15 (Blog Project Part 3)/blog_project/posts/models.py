05350b3c4c4da2b32679690e2dbbb85aed8f9b40
Khaled Karim <kskarim177@gmail.com>
2024-09-03 04:26:17 +0600

    DetailsViewPage And Comment Section

--- Back-end/Django codes/Module 15 (Blog Project Part 3)/blog_project/posts/models.py
+++ Back-end/Django codes/Module 15 (Blog Project Part 3)/blog_project/posts/models.py
@@ -6,9 +6,18 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    category = models.ManyToManyField(Category)  # One post can belong to multiple categories and vise versa
+    category = models.ManyToManyField(Category) # One post can belong to multiple categories and vise versa
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='posts/media/uploads/',blank=True,null=True)
-
+    image = models.ImageField(upload_to='posts/media/uploads/', blank = True, null = True)
     def __str__(self):
-        return f'{self.title} '
+        return self.title 
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    body = models.TextField(null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True) 
+    
+    def __str__(self):
+        return f"Comments by {self.name}"
